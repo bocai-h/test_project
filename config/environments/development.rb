dba11eb452d029c99bfad96c078131bf145a136b
bocai <xiaobo.huang@hand-china.com>
2017-06-08 00:56:42 +0800

    测试使用本地gem

---
+++ config/environments/development.rb
@@ -0,0 +1,70 @@
+RELOAD_GEM = lambda do |gem|
+  # $"会输出所有载入内存的ruby文件地址
+  $".grep(Regexp.new(gem)).each do |file|
+    # 从新载入和gem相关的所有ruby文件
+    load file
+  end
+end
+
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+
+  # rails正常情况下会监听autoload路径下文件的改动然后重新加载,该设置就是让每次请求过来rails都重新加载所有文件
+  config.reload_classes_only_on_change = false
+  # 调用rails的钩子方法 该方法在autoload_path路径下的文件改变时会被触发
+  config.to_prepare do
+    RELOAD_GEM.call 'gem_in_action'
+  end
+
+  config.cache_classes = false
+
+  # Do not eager load code on boot.
+  config.eager_load = false
+
+  # Show full error reports.
+  config.consider_all_requests_local = true
+
+  # Enable/disable caching. By default caching is disabled.
+  if Rails.root.join('tmp/caching-dev.txt').exist?
+    config.action_controller.perform_caching = true
+
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => 'public, max-age=172800'
+    }
+  else
+    config.action_controller.perform_caching = false
+
+    config.cache_store = :null_store
+  end
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_caching = false
+
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
+
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
+
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
+
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
+byebug
+  # Use an evented file watcher to asynchronously detect changes in source code,
+  # routes, locales, etc. This feature depends on the listen gem.
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+end
